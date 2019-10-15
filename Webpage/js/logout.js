d14a6d1febcfd979f4e7a55225667563a3fdf24c
Sebastian Pehlke <>
2019-10-15 14:35:30 +0200

    Fix logout

--- Webpage/js/logout.js
+++ Webpage/js/logout.js
@@ -1,30 +1,5 @@
 
 function onLogout() {
-	var data = "";
-	
-	$.ajax({
-		url: url + port + "/user/login", 					// Request URL							
-		type: "post",
-		data: data,
-		success: function ()   	//  function called when succeded
-		{
-	
-			console.log('loggedOut');	
-			jwtString = "";
-			document.cookie = "" ;
-			window.location.href = 'Homepage.html';
-			//getAllItems();
-		},
-		error: function (XMLHttpRequest, textStatus, errorThrown) {
-			console.log('fehler');
-		}
-
-
-
-
-
-
-console.log('loggedOut');
-
-window.location.href = 'Homepage.html';
+	document.cookie = "jwt = ";
+	window.location.href = 'Homepage.html';
 }
